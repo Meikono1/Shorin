9de1783371ed77b3e569af6e035a69f36ca2fc89
Meikono <c.eppers122@googlemail.com>
2019-07-28 15:37:45 +0200

    Version 0.1 Hauptbildschirm in arbeit

--- src/com/eppersindustries/shorin/Main.java
+++ src/com/eppersindustries/shorin/Main.java
@@ -1,8 +1,50 @@
 package com.eppersindustries.shorin;
 
-public class Main {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private String patchnotes = "Shorin Patch:0.1";
+    private Insets buttonpadding = new Insets(10, 10, 10, 10);
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setTitle("Shorin");
+
+        BorderPane startbildschirm = new BorderPane();
+        startbildschirm.setPrefHeight(700);
+        startbildschirm.setPrefWidth(900);
+        Label patch = new Label();
+        startbildschirm.setTop(patch);
+
+        HBox boxone = new HBox();
+        boxone.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        boxone.setSpacing(10);
+        boxone.setPrefHeight(100);
+        boxone.alignmentProperty();
+
+        Button start = new Button("Game Start");
+        Button laden = new Button("Load Game");
+        Button optionen = new Button("Optionen");
+
+        boxone.getChildren().addAll(start, laden, optionen);
+
+        startbildschirm.setBottom(boxone);
+        patch.setText(patchnotes);
+
+        stage.setScene(new Scene(startbildschirm));
+        stage.show();
     }
 }
